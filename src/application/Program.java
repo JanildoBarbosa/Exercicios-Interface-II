a2b4a4a542568c0940617b844b3a461b1cfc4078
Janildo Barbosa <janildofbabosa@gmail.com>
2023-01-05 19:32:17 -0300

    Finished class ContractService

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,16 @@ package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.ContractService;
+import model.entities.Installment;
 
 public class Program {
 
@@ -14,16 +21,35 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		//LocalDate date;
+		
+		List<Installment>installments = new ArrayList<>();
 		
 		System.out.println("Entre os dados do contrato:");
 		System.out.print("Numero: ");
+		int number = sc.nextInt();
 		System.out.print("Data (dd/MM/yyyy): ");
-		Date date = sdf.parse(sc.nextLine());
+		LocalDate.parse(sc.nextLine());
 		System.out.println("Valor do contrato: ");
 		double contractValue = sc.nextDouble();
+		
 		System.out.println("Entre com o numero de parcelas: ");
 		int installmentQuantity = sc.nextInt(); 
+		LocalDate date = LocalDate.parse(sc.next(), dtf);
+		date = LocalDate.now().plusDays(30);
+		//date = LocalDate.now().plusDays(30);
 		
+		Contract contract = new Contract(number, date, contractValue);
+		
+		ContractService contractService = new ContractService()
+
+		
+		
+		
+
+		
+
 		sc.close();
 
 	}
--- src/model/entities/ContractService.java
+++ src/model/entities/ContractService.java
@@ -1,8 +1,25 @@
 package model.entities;
 
+import java.time.LocalDate;
+
 public class ContractService {
 	
 	public void processContract(Contract contract, Integer months) {
+		
+		LocalDate dueDate;
+		double amount = 0.0;
+		double capital = contract.getTotalValue() / months;
+		double tax = 0.02 * capital;
+		
+		for (int i = 0; i < months; i++) {
+			
+			dueDate = contract.getDate().plusDays(30);
+
+			amount = capital + tax * i;
+			
+			contract.setInstallments(new Installment(dueDate, amount));
+			
+		}
 		
 	}
 
